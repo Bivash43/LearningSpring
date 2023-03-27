3e37a2d654493c3128fb35065560de0ac6a00f63
Bivash43 <bivash43e@gmail.com>
2023-03-27 11:30:01 +0545

    CRUD retrieve

--- src/main/java/com/studentservice/student/controller/StudentController.java
+++ src/main/java/com/studentservice/student/controller/StudentController.java
@@ -3,9 +3,13 @@ package com.studentservice.student.controller;
 import com.studentservice.student.model.Student;
 import com.studentservice.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -16,6 +20,12 @@ public class StudentController {
     public Student saveStudent(@RequestBody Student student){
         return studentService.saveStudent(student);
     }
+
+    @GetMapping("/student/getAll")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        return ResponseEntity.ok(studentService.getAllStudent());
+    }
+
 
 
 }
--- src/main/java/com/studentservice/student/service/StudentService.java
+++ src/main/java/com/studentservice/student/service/StudentService.java
@@ -2,7 +2,10 @@ package com.studentservice.student.service;
 
 import com.studentservice.student.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudent();
 }
